99776ea0e31d21a9b27784314939ed17ea23ed18
NassimBenattou <mohamed-nassim.benattou@epitech.eu>
2023-11-05 20:55:33 +0100

    add production version

---
+++ build/precache-manifest.bda90c0681be3fe82cd68c8a005ee158.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7a91bbf963f3b3442d85842d9c67d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5730c5f9bd9e4df3131",
+    "url": "/static/css/main.cf311b17.chunk.css"
+  },
+  {
+    "revision": "e4ee83181ec32e103787",
+    "url": "/static/js/2.9cc4c94c.chunk.js"
+  },
+  {
+    "revision": "11211063c65e8c9340e3ded9e980210e",
+    "url": "/static/js/2.9cc4c94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baad14ce4792148f6bdd",
+    "url": "/static/js/3.3cc0aca0.chunk.js"
+  },
+  {
+    "revision": "c5730c5f9bd9e4df3131",
+    "url": "/static/js/main.993b833b.chunk.js"
+  },
+  {
+    "revision": "f28946a380bd196a8a0e",
+    "url": "/static/js/runtime-main.955e15f2.js"
+  },
+  {
+    "revision": "907a56ba15742899a6dfc12238d7ede2",
+    "url": "/static/media/benattou.907a56ba.jpeg"
+  }
+]);
