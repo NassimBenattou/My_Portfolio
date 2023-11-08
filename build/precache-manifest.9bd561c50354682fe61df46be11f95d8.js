f23841dfe6a6d1350f724dd2c4e4ed9890b04932
NassimBenattou <mohamed-nassim.benattou@epitech.eu>
2023-11-08 21:35:51 +0100

    delete build

---
+++ build/precache-manifest.9bd561c50354682fe61df46be11f95d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae38df93666aa2f15e785f812dc9376",
+    "url": "/My_portfolio/index.html"
+  },
+  {
+    "revision": "15e17eac6e5f11b3b063",
+    "url": "/My_portfolio/static/css/main.3028394c.chunk.css"
+  },
+  {
+    "revision": "d1d4c7f0aa3fe239387e",
+    "url": "/My_portfolio/static/js/2.a4335dba.chunk.js"
+  },
+  {
+    "revision": "9a17e25e3849a7d3fcad27ccef145dbb",
+    "url": "/My_portfolio/static/js/2.a4335dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973e1ed2941885c8892a",
+    "url": "/My_portfolio/static/js/3.9f97b289.chunk.js"
+  },
+  {
+    "revision": "15e17eac6e5f11b3b063",
+    "url": "/My_portfolio/static/js/main.bc726be1.chunk.js"
+  },
+  {
+    "revision": "56b699d8d082ac23673f",
+    "url": "/My_portfolio/static/js/runtime-main.1829e104.js"
+  },
+  {
+    "revision": "907a56ba15742899a6dfc12238d7ede2",
+    "url": "/My_portfolio/static/media/benattou.907a56ba.jpeg"
+  }
+]);
